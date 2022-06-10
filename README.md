e9ca4522e12816c2a20031c508f98f8f40d88687
Silbad <psilbad@gmail.com>
2022-06-10 23:18:50 +0200

    Update base repo for GitHub Pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vue-tailwind-player
 
 A web audio player with vue3 &amp; tailwind
+
+https://vitejs.dev/guide/static-deploy.html#testing-the-app-locally
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     plugins: [
         vue(),
     ],
+    base: '/vue-tailwind-player/',
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     },
